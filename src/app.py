31b1c187b993fc9cfa0c6e17684071d941fb0ce3
Julian <juliansanchezave@gmail.com>
2024-10-26 19:18:45 -0500

    interfaz

--- src/app.py
+++ src/app.py
@@ -9,6 +9,30 @@ if __name__ == '__main__':
 def index():
     return render_template('index.html')
 
-@flask.route('/otro')
-def segunda():
-    return render_template('otro.html')
+@flask.route('/index')
+def login():
+    return render_template('index.html')
+
+@flask.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
+@flask.route('/clientes')
+def clientes():
+    return render_template('clientes.html')
+
+@flask.route('/facturas')
+def facturas():
+    return render_template('facturas.html')
+
+@flask.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@flask.route('/cuenta')
+def cuenta():
+    return render_template('cuenta.html')
+
+
+
+
